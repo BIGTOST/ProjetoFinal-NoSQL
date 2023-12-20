28d9edd4c71e49f107279cb056c843275e9c7b1a
(Alex)andre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2023-12-20 10:17:38 +0000

    Final Deploy

--- app/src/main/java/projetofinal/App.java
+++ app/src/main/java/projetofinal/App.java
@@ -15,24 +15,205 @@ public class App {
             Cinema movies = new Cinema(database);
             System.out.println("Bem Vindo ao Cinemas Nos");
             while(true){
-                System.out.print("\033[H\033[2J");
+                //System.out.print("\033[H\033[2J");
+                //*Menu Inicial
                 System.out.println("\nMenu Inicial");
-                System.out.println("1- Listar Todos os Filmes");
+                System.out.println("1- Listagens");
                 System.out.println("2- Introduzir filmes");
                 System.out.println("3- Atualizar Filmes");
                 System.out.println("4- Remover Filmes");
-                System.out.println("5- Listar Todos os Filmes");
                 
                 
                 System.out.println("0- Encerrar");
                 System.out.println("Selecione a opção");
 
                 int opcao = scanner.nextInt();
-                System.out.print("\033[H\033[2J");
+
                 switch(opcao){
                     case 1 ->{
+                        boolean sair = true ;
+                        int op = 20;
+                        while (sair) {
+                            //*Submenu
+                            scanner.nextLine();
+                            System.out.println("\nMenu Inicial");
+                            System.out.println("x- Listagens");
+                            System.out.println("\t1-Listar Todos");
+                            System.out.println("\t2-Encontrar Nome");
+                            System.out.println("\t3-Listar filmes por Ano");
+                            System.out.println("\t4-Ano com mais filmes");
+                            System.out.println("x- Introduzir filmes");
+                            System.out.println("x- Atualizar Filmes");
+                            System.out.println("x- Remover Filmes");
+                            System.out.println("0- voltar");
+                            System.out.println("Selecione a opção");
+                            op = scanner.nextInt();
+                            switch (op) {
+                                case 1->{
+                                    List<Document> listaDeFilmes = movies.listarMovies();
+                                    if (listaDeFilmes.isEmpty())
+                                        System.out.println("Cinema Vazio :(");
+                                    else{
+                                        for (Document moviDocument : listaDeFilmes) {
+                                            System.out.println("\n");
+                                           for (Map.Entry<String, Object> entry : moviDocument.entrySet()) {
+                                                System.out.println(entry.getKey() + ": " + entry.getValue());
+                                            }
+                                        }
+                                    }
 
-                    } 
+                                }
+                                case 2->{
+                                    scanner.nextLine();
+                                    System.out.println("Indique o nome do filme a pesquisar: ");
+                                    String name = scanner.nextLine();
+                                    Document movie = movies.findMovieByName(name);
+                                    //System.out.println(car.toJson());
+                                    System.out.println("\n");
+                                    for (Map.Entry<String, Object> entry : movie.entrySet()) {
+                                        System.out.println(entry.getKey() + ": " + entry.getValue());
+                                    }
+                                }
+                                case 3->{
+                                    //* SubSubmenu
+                                    scanner.nextLine();
+                                    System.out.println("Indique o ano: ");
+                                    int ano = scanner.nextInt();
+                                    System.out.println("\nMenu Inicial");
+                                    System.out.println("x- Listagens");
+                                    System.out.println("\txx-Listar Todos");
+                                    System.out.println("\txx-Encontrar Nome");
+                                    System.out.println("\txx-Listar filmes por Ano");
+                                    System.out.println("\t\t1-Antes de "+ ano);
+                                    System.out.println("\t\t2-Depois de "+ ano);
+                                    System.out.println("\t\t3-Em "+ ano);
+                                    System.out.println("x- Introduzir filmes");
+                                    System.out.println("x- Atualizar Filmes");
+                                    System.out.println("x- Remover Filmes");
+                                    System.out.println("0- voltar");
+                                    System.out.println("Selecione a opção");
+                                    op = scanner.nextInt();
+                                    switch (op) {
+                                        case 1->{
+                                            List<Document> listaDeFilmes = movies.listarMoviesAntesDe(ano);
+                                            if (listaDeFilmes.isEmpty())
+                                                System.out.println("Nenhum Filme antes desse ano:(");
+                                            else{
+                                                for (Document moviDocument : listaDeFilmes) {
+                                                    System.out.println("\n");
+                                                for (Map.Entry<String, Object> entry : moviDocument.entrySet()) {
+                                                        System.out.println(entry.getKey() + ": " + entry.getValue());
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        case 2->{
+                                            List<Document> listaDeFilmes = movies.listarMoviesDespoisDe(ano);
+                                            if (listaDeFilmes.isEmpty())
+                                                System.out.println("Nenhum Filme depois desse ano:(");
+                                            else{
+                                                for (Document moviDocument : listaDeFilmes) {
+                                                    System.out.println("\n");
+                                                for (Map.Entry<String, Object> entry : moviDocument.entrySet()) {
+                                                        System.out.println(entry.getKey() + ": " + entry.getValue());
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        case 3->{
+                                            List<Document> listaDeFilmes = movies.listarMoviesEm(ano);
+                                            if (listaDeFilmes.isEmpty())
+                                                System.out.println("Nenhum Filme nesse ano:(");
+                                            else{
+                                                for (Document moviDocument : listaDeFilmes) {
+                                                    System.out.println("\n");
+                                                for (Map.Entry<String, Object> entry : moviDocument.entrySet()) {
+                                                        System.out.println(entry.getKey() + ": " + entry.getValue());
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                case 4 ->{
+                                    movies.encontrarAnoComMaisFilmes();
+                                }
+                                case 0->{
+                                    sair = false;
+                                }
+                                default->{
+                                    System.out.println("Opção não encontrada");
+                                }
+                            }
+                        }
+                    }
+                    case 2 -> {
+                        scanner.nextLine();
+                        System.out.println("Quantos Deseja Inserir: ");
+                        int Numero = scanner.nextInt();
+                        if(Numero>1){
+                            for (int i = 0; i < Numero; i++) {
+                                System.out.println("Indique o Titulo do Filme: ");
+                                String Titulo = scanner.nextLine();
+                                System.out.println("Indique o nome do Produtor: ");
+                                String nomeProdutor = scanner.nextLine();
+                                System.out.println("Indique o genero do Produtor: ");
+                                String genero = scanner.nextLine();
+                                System.out.println("Indique o ano de lançamento: ");
+                                int anoDeLaçamento = scanner.nextInt();
+                                scanner.nextLine();
+                                System.out.println("Indique o tempo de tela: ");
+                                int tempoDeTela = scanner.nextInt();
+
+                                Document newMovie = new Document("Titulo", Titulo)
+                                        .append("Produtor", nomeProdutor)
+                                        .append("genero", genero)
+                                        .append("anoDeLançamento", anoDeLaçamento)
+                                        .append("Tempo de Tela", tempoDeTela);
+                                movies.addMovie(newMovie);
+                            }
+                        }else{
+                            System.out.println("Indique o Titulo do Filme: ");
+                            String Titulo = scanner.nextLine();
+                            System.out.println("Indique o nome do Produtor: ");
+                            String nomeProdutor = scanner.nextLine();
+                            System.out.println("Indique o genero do Produtor: ");
+                            String genero = scanner.nextLine();
+                            System.out.println("Indique o ano de lançamento: ");
+                            int anoDeLaçamento = scanner.nextInt();
+                            scanner.nextLine();
+                            System.out.println("Indique o tempo de tela: ");
+                            String tempoDeTela = scanner.nextLine();
+
+                            Document newMovie = new Document("Titulo", Titulo)
+                                    .append("Produtor", nomeProdutor)
+                                    .append("genero", genero)
+                                    .append("anoDeLançamento", anoDeLaçamento)
+                                    .append("Tempo de Tela", tempoDeTela);
+                            movies.addMovie(newMovie);
+                        }
+                    }
+                    case 3 ->{
+                        scanner.nextLine();
+                        System.out.println("Indique o id do filme que deseja atualizar: ");
+                        String id = scanner.nextLine();
+
+                        System.out.println("Qual o campo a atualizar");
+                        String field = scanner.nextLine();
+
+                        System.out.println("Escreva o valor do campo: ");
+                        String value = scanner.nextLine();
+
+
+                        movies.updateMovieValue(id, field, value);
+                    }
+                    case 4 ->{
+                        scanner.nextLine();
+                        System.out.println("Indique o id do movie que deseja apagar: ");
+                        String id = scanner.nextLine();
+
+                        movies.deleteMovieById(id);
+                    }
                     case 0 ->{
                         System.out.println("A sair do Cinema");
                         return;
--- app/src/main/java/projetofinal/Cinema.java
+++ app/src/main/java/projetofinal/Cinema.java
@@ -4,23 +4,178 @@ import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.gt;
+import static com.mongodb.client.model.Filters.lt;
 import static com.mongodb.client.model.Updates.set;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 
 public class Cinema {
     private final MongoCollection<Document> cinemaMovies;
 
     public Cinema(MongoDatabase database){
-    this.cinemaMovies = database.getCollection("movies");
+        this.cinemaMovies = database.getCollection("movies");
+    }
+    Bson projection = Projections.exclude("_id");
+
+    public void addMovie(Document movie) {
+        try {
+            cinemaMovies.insertOne(movie);
+            System.out.println("movie added successfully.");
+        } catch (MongoException e) {
+            System.err.println("Erro ao adicionar o movie: " + e.getMessage());
+        }
+    }
+
+    public Document findMovieById(String id) {
+        try {
+            
+            ObjectId objectId = new ObjectId(id);
+            Document movie = cinemaMovies
+            .find(eq("_id", objectId))
+            .projection(projection)
+            .first();
+
+            if (movie != null) {
+                return movie;
+            } else {
+                System.out.println("Nenhum filme encontrado com esse codigo " + id);
+                return null;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Formato de ID invalido: " + id);
+            return null;
+        }    
+	}
+
+    public Document findMovieByName(String name) {
+        try {
+            Document movie = cinemaMovies
+            .find(eq("Titulo", name))
+            .projection(projection)
+            .first();
+
+            if (movie != null) {
+                return movie;
+            } else {
+                System.out.println("Nenhum filme encontrado com esse nome " + name);
+                return null;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Formato de Nome invalido: " + name);
+            return null;
+        }    
+	}
+
+    //* Listagem de todos os filmes na tabela para uma lista
+    public List<Document> listarMovies() {
+        List<Document> movies = cinemaMovies
+        .find()
+        .projection(projection)
+        .into(new ArrayList<>());
+    
+        // Log the count of movies retrieved
+        System.out.println("Number of movies retrieved: " + movies.size());
+
+        return movies;
+    }
+    public List<Document> listarMoviesDespoisDe(int ano) {
+        List<Document> movies = cinemaMovies
+        .find(gt("anoDeLançamento",ano))
+        .projection(projection)
+        .into(new ArrayList<>());
+    
+        // Log the count of movies retrieved
+        System.out.println("Number of movies retrieved: " + movies.size());
+
+        return movies;
+    }
+    public List<Document> listarMoviesAntesDe(int ano) {
+        List<Document> movies = cinemaMovies
+        .find(lt("anoDeLançamento",ano))
+        .projection(projection)
+        .into(new ArrayList<>());
+    
+        // Log the count of movies retrieved
+        System.out.println("Number of movies retrieved: " + movies.size());
+
+        return movies;
+    }
+    public List<Document> listarMoviesEm(int ano) {
+        List<Document> movies = cinemaMovies
+        .find(eq("anoDeLançamento",ano))
+        .projection(projection)
+        .into(new ArrayList<>());
+    
+        // Log the count of movies retrieved
+        System.out.println("Number of movies retrieved: " + movies.size());
+
+        return movies;
+    }
+    
+    //* update Movie por id
+    public void updateMovieValue(String id, String field, String value) {
+
+        if(field == "anoDeLançamento" || field =="Tempo de Tela"){
+            int altValue =Integer.valueOf(value);
+            try {
+                ObjectId objectId = new ObjectId(id);
+                cinemaMovies.updateOne(eq("_id", objectId), set(field, altValue));
+                System.out.println("Car updated successfully.");
+            } catch (Exception e) {
+                System.out.println("Error updating car: " + e.getMessage());
+            }
+        }else{
+            try {
+                ObjectId objectId = new ObjectId(id);
+                cinemaMovies.updateOne(eq("_id", objectId), set(field, value));
+                System.out.println("Car updated successfully.");
+            } catch (Exception e) {
+                System.out.println("Error updating car: " + e.getMessage());
+            }
+        }
+    }
+    
+    //* delet movie por id
+    public void deleteMovieById(String id) {
+        try {
+            ObjectId objectId = new ObjectId(id);
+            cinemaMovies.deleteOne(eq("_id", objectId));
+            System.out.println("Car deleted successfully.");
+        } catch (Exception e) {
+            System.out.println("Error deleting car: " + e.getMessage());
+        }
+    }
+
+    public Document encontrarAnoComMaisFilmes() {
+        Bson groupStage = Aggregates.group("$anoDeLançamento", Accumulators.sum("count", 1));
+        Bson sortStage = Aggregates.sort(Sorts.descending("count"));
+        Bson limitStage = Aggregates.limit(1);
+
+        List<Document> resultado = cinemaMovies.aggregate(Arrays.asList(groupStage, sortStage, limitStage)).into(new ArrayList<>());
+
+        if (!resultado.isEmpty()) {
+            return resultado.get(0);
+        } else {
+            System.out.println("Nenhum filme encontrado.");
+            return null;
+        }
     }
 }
